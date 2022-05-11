847e63cd5f749f83c4958b276247ed34e1d11ad4
LuisDElgado <luisdelgadobravo652@gmail.com>
2022-05-10 21:35:41 -0500

    firts

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'package:cartoons_flutter/model/characters_data.dart';
+import 'package:cartoons_flutter/widgets/character_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cartoooooonery'),
+      ),
+      body: ListView(
+        children: List.generate(characters.length, (index) {
+          return CharacterWidget(character: characters[index]);
+        }),
+      ),
+    );
+  }
+}
